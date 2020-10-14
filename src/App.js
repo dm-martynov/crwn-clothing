d0362d948e528a517b961a23b6a3cdd231a97533
dmitrii_martynov <dmitriimartynov1@gmail.com>
2020-10-14 10:44:51 +0300

    added sign-in form and button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
-import Header from './components/header/header.component.jsx'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import Header from './components/header/header.component'
 
 
 
@@ -13,10 +14,11 @@ function App() {
     <div>
     <Header />
     <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={ShopPage} />
-      </Switch>
-    </div>
+    <Route exact path='/' component={HomePage} />
+    <Route exact path='/shop' component={ShopPage} />
+    <Route exact path='/signin' component={SignInAndSignUpPage} />
+    </Switch>
+      </div>
   );
 }
 
